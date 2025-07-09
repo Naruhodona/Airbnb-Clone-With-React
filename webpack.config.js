9a734518c4b61c8e9b9dc4f96a7b63bd04827f67
Naruhodona <fredrikfernando2018@gmail.com>
2025-07-09 13:56:56 +0700

    Init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+  mode: "development",
+};
